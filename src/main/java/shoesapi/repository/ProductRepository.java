526b3f4dc519c0a228e49101ce495f1c0784a1df
Nguyen Gia Phu <104088012+DracoVanVavidar@users.noreply.github.com>
2025-06-17 21:59:31 +0700

    Delete src/main/java/shoesapi/repository directory

--- src/main/java/shoesapi/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.shoesapi.repository;
-
-import com.example.shoesapi.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
