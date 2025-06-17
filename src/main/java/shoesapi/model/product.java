574ee88b11c23b2abea7922d25a81b32636e562d
Nguyen Gia Phu <104088012+DracoVanVavidar@users.noreply.github.com>
2025-06-17 21:43:22 +0700

    Create product.java

---
+++ src/main/java/shoesapi/model/product.java
@@ -0,0 +1,11 @@
+package com.example.shoesapi.model;
+
+public class Product {
+    private String name;
+    private double price;
+    private String brand;
+    private String description;
+    private String imageUrl;
+
+    // Constructors, getters, setters
+}
